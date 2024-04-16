91e0634f1bcb6b26cf71ebcd2bf132245f9c3bb7
Brandon McCarthy <Bmac96.dev@gmail.com>
2024-04-16 19:56:13 -0400

    Solved problems 1-6

---
+++ problems/17-bonus-permutations.js
@@ -0,0 +1,44 @@
+/***********************************************************************
+ * BONUS PROBLEM
+************************************************************************
+Write a recursive method permutations(array) that calculates all the
+permutations of the given array. For an array of length n there are n! different
+permutations. So for an array with three elements we will have 3 * 2 * 1 = 6
+different permutations.
+
+Examples:
+
+permutations([1, 2]) // [[1, 2], [2, 1]]
+permutations([1, 2, 3]) // [[1, 2, 3], [1, 3, 2],
+                        // [2, 1, 3], [2, 3, 1],
+                        // [3, 1, 2], [3, 2, 1]]
+***********************************************************************/
+
+const permutations = (array) => {
+    const result = [];
+
+    const generatePermutations = (arr, start) => {
+        if (start === arr.length - 1) {
+            result.push([...arr]);
+            return;
+        }
+
+        for (let i = start; i < arr.length; i++) {
+            [arr[start], arr[i]] = [arr[i], arr[start]];
+
+            generatePermutations(arr, start + 1);
+
+            [arr[start], arr[i]] = [arr[i], arr[start]];
+        }
+    };
+    generatePermutations(array, 0);
+
+    return result;
+};
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+    module.exports = permutations;
+} catch (e) {
+    module.exports = null;
+}
